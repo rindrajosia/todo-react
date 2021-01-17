e70f09e607365d5b75562cae90b8f0fe0f281d5a
rindra josia <rrindrajosia@gmail.com>
2021-01-17 21:43:03 +0300

    Build Tasks and Checkbox component

---
+++ src/components/Checkbox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const Checkbox = ({ id }) => {
+  const archiveTask = () => {
+    firebase
+    .firestore()
+    .collection('tasks')
+    .doc(id)
+    .update({
+      archive: true,
+    });
+  };
+
+  return (
+    <div
+      className="checkbox-holder"
+      data-testid="checkbox-action"
+      onClick={() => archiveTask()}
+    >
+      <span className="checkbox" />
+    </div>
+  )
+}
